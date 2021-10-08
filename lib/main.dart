76327452567def46fc47f2454aa59209149ff93e
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-10-07 22:43:08 -0300

    Implementação de provider para tabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'fooderlich_theme.dart';
 import 'home.dart';
+import 'package:provider/provider.dart';
+import 'models/models.dart';
 
 void main() {
   runApp(const Fooderlich());
@@ -16,8 +17,13 @@ class Fooderlich extends StatelessWidget {
     return MaterialApp(
       theme: theme,
       title: 'Fooderlich',
-      // TODO 8: Replace this with MultiProvider
-      home: const Home(),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          // TODO 10: Add GroceryManager Provider
+        ],
+        child: const Home(),
+      ),
     );
   }
 }
