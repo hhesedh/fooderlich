738415fe809621171f74a6b7b7b6daa4c4f1173d
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-10-08 05:26:25 -0300

    criação de items na tela de detalhe

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/models/grocery_manager.dart';
 import 'package:provider/provider.dart';
-
+import 'grocery_item_screen.dart';
 import 'empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
@@ -13,7 +13,19 @@ class GroceryScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          // TODO 11: Present GroceryItemScreen
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GroceryItemScreen(
+                onCreate: (item) {
+                  manager.addItem(item);
+                  Navigator.pop(context);
+                },
+                onUpdate: (item) {},
+              ),
+            ),
+          );
         },
       ),
       body: buildGroceryScreen(),
